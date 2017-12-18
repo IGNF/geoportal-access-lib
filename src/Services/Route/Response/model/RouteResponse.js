59410383ea0a4cb2fe2a97d242db5a7275ca3e7f
lboulanger <laurane.boulanger@ign.fr>
2017-12-18 14:52:49 +0100

    fix doc Gp.Services.route() : wrong type for totalDistance (#48)

--- src/Services/Route/Response/model/RouteResponse.js
+++ src/Services/Route/Response/model/RouteResponse.js
@@ -8,7 +8,7 @@ define([], function () {
      * @property {Gp.BBox} bbox - Bounding Box of the route. Given when provideBBox parameter is used in function call.
      * @property {Object} routeGeometry - Geometry (expressed in [GeoJSON]{@link http://geojson.org/}) of the route.
      * @property {Array.<Gp.Services.Route.RouteInstruction>} routeInstructions - Instructions of the route.
-     * @property {Float} totalDistance - Length of the route. Expressed in km or m, depending on distanceUnit parameter.
+     * @property {String} totalDistance - Length of the route. If distanceUnit parameter was set to "km" (default), totalDistance is a string containing the total distance expressed in kilometers, followed by " Km" (e.g. : "19.6 Km"). If distanceUnit parameter was set to "m", totalDistance is a string containing the total distance expressed in meters (e.g. : "19599.14").
      * @property {Float} totalTime - Route duration in seconds.
      *
      * @namespace
