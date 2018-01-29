c97ba8fbca7765d9ad4b0883687985316ac43d9c
lowzonenose <jpbazonnais@gmail.com>
2018-01-29 15:18:37 +0100

    Revert "maj du template UMD (#53)"
    This reverts commit 8a5abb52b7a4fecb942aea21ea9cca37bdfbfe3b.

--- samples/Services/Alti/ES6/es6-module.js
+++
@@ -1,6 +0,0 @@
-import * as Gp from "../../../../dist/GpServices-src.js";
-
-export function compute (options) {
-    // call self = window to get Gp (globale !)
-    self.Gp.Services.getAltitude(options);
-}
