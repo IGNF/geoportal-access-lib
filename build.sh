742e5fbae2d21ccc62510946f5cd784222b69a5d
lowzonenose <jpbazonnais@gmail.com>
2018-02-26 15:44:58 +0100

    release 1.2.0

--- build.sh
+++ build.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-# Script de construction des bundles ainsi que de deploiement
-# sur le serveur de sources
-#  - TODO choix des bundles à construire
-#  - TODO mise en place d'un "Build Number" unique et commune à tous les bundles
-#         ex. fichier build.number avec un numéro qui est incrementé aprés chaque execution
-#  - TODO gulp publish --build-number
-#  - TODO gérer la publication des bundles sur le depot de sources (mercurial ou git)
-#         hg commit -m "building bundles ${build.number}..."
-#         hg outgoing | sed -n -e 3p  | cut -d: -f3 (ex. 944ed81fd2b6)
-#         hg push
+# Script de construction des bundles
 
 echo "BEGIN"
 
