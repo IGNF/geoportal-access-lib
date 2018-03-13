97140ccd41778d8a3cf238eb51d8f2a40087ee16
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-03-13 12:00:32 +0000

    Deploy IGNF/geoportal-access-lib to github.com/IGNF/geoportal-access-lib.git:gh-pages

--- current/dist/GpServices-debug.js
+++ current/dist/GpServices-debug.js
@@ -10,7 +10,7 @@
  * copyright IGN
  * @author IGN
  * @version 1.2.0
- * @date 2018-02-28
+ * @date 2018-03-13
  *
  */
 /*!
@@ -10106,7 +10106,7 @@ Gp = function (XHR, Services, AltiResponse, Elevation, AutoCompleteResponse, Sug
     var scope = typeof window !== 'undefined' ? window : {};
     var Gp = scope.Gp || {
         servicesVersion: '1.2.0',
-        servicesDate: '2018-02-28',
+        servicesDate: '2018-03-13',
         extend: function (strNS, value) {
             var parts = strNS.split('.');
             var parent = this;
