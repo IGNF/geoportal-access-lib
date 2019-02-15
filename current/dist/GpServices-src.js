d7bc38805d9bc645bdff2f46a7efa77475063b15
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-15 23:42:16 +0000

    Deploy IGNF/geoportal-access-lib to github.com/IGNF/geoportal-access-lib.git:gh-pages

--- current/dist/GpServices-src.js
+++ current/dist/GpServices-src.js
@@ -10,7 +10,7 @@
  * copyright IGN
  * @author IGN
  * @version 2.1.2
- * @date 2019-02-14
+ * @date 2019-02-15
  *
  */
 /*!
@@ -5430,7 +5430,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 var Gp = {
     servicesVersion : "2.1.2",
-    servicesDate : "2019-02-14",
+    servicesDate : "2019-02-15",
     /**
      * Methode pour rajouter une classe / objet au namespace global.
      *
