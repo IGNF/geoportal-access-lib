cc34a87de9ec38b8d1b2555f83261b98242a873f
lgrd <loic.grondin.pro@gmail.com>
2020-03-26 11:20:38 +0100

    feat(docker): gestion des tests end-to-end dans docker

--- docker/scripts/start.sh
+++ docker/scripts/start.sh
@@ -2,4 +2,5 @@
 
 service nginx start
 bash /home/docker/watch.sh &
-cd /home/docker/geoportal-access-lib/ && npm run test:serve:docker 
+cd /home/docker/geoportal-access-lib/ && npm run test:serve:docker &
+cd /home/docker/geoportal-access-lib/ && npm run test:serve:end-to-end:docker &
