e53e0f6e88ee837a5e1479cafadd5420fd336c03
lgrd <loic.grondin.pro@gmail.com>
2020-03-26 11:58:16 +0100

    fix(docker): correction du script start.sh pour les tests end-to-end

--- docker/scripts/start.sh
+++ docker/scripts/start.sh
@@ -3,4 +3,4 @@
 service nginx start
 bash /home/docker/watch.sh &
 cd /home/docker/geoportal-access-lib/ && npm run test:serve:docker &
-cd /home/docker/geoportal-access-lib/ && npm run test:serve:end-to-end:docker &
+cd /home/docker/geoportal-access-lib/ && npm run test:end-to-end:serve:docker 
