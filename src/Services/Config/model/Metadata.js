2068eb46053fdbdd6c98eb632c4e2efd32b6821a
jean-philippe.bazonnais <jean-philippe.bazonnais@ign.fr>
2024-08-21 18:51:29 +0200

    Ajout de la jsdoc pour le service de configuration

---
+++ src/Services/Config/model/Metadata.js
@@ -0,0 +1,12 @@
+
+/**
+ * Object used to describe a metadata file associated with a geoportal resource.
+ *
+ * @property {String} format - mime-type of the metadata file.
+ * @property {String} url - metadata file URL
+ *
+ * @namespace
+ * @alias Gp.Services.Config.Metadata
+ */
+
+var Metadata = {};
---
+++ src/Services/Config/model/Thematic.js
@@ -0,0 +1,12 @@
+
+/**
+ * Object used to describe a thematic
+ *
+ * @property {String} name - Thematic name
+ * @property {Boolean} inspire - true if the thematic name is an [INSPIRE one]{@link http://inspire.ec.europa.eu/index.cfm/pageid/2/list/7}.
+ *
+ * @namespace
+ * @alias Gp.Services.Config.Thematic
+ */
+
+var Thematic = {};
